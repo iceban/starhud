2b60d0ea9698de433908a715b9e19d7b32039a59
ajayanjay <muhammad.azzar.tif424@polban.ac.id>
2025-06-30 20:57:15 +0700

    X Coordinate is actually showing the wrong color, I hate this.

--- src/main/java/fin/starhud/config/hud/CoordSettings.java
+++ src/main/java/fin/starhud/config/hud/CoordSettings.java
@@ -12,7 +12,7 @@ public class CoordSettings {
     public BaseHUDSettings base = new BaseHUDSettings(true, 5, 5, ScreenAlignmentX.LEFT, ScreenAlignmentY.TOP);
 
     @ConfigEntry.Gui.CollapsibleObject
-    public CoordPieceSetting X = new CoordPieceSetting(true, 0, 0,0xFc7817);
+    public CoordPieceSetting X = new CoordPieceSetting(true, 0, 0,0xFc7871);
 
     @ConfigEntry.Gui.CollapsibleObject
     public CoordPieceSetting Y = new CoordPieceSetting(true, 0, 14,0xA6F1AF);
